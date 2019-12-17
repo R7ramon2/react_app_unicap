c69729c6585f174283289b13d883e9351fe7ce7e
Ramon Ranieri <ramonranieri@hotmail.com>
2019-12-17 01:32:44 -0300

    getLivros

--- back-end/livros.php
+++ back-end/livros.php
@@ -1,26 +1,15 @@
 <?php
-// header('Content-Type: text/html; charset=iso');
-// header('Content-Type: application/json');
 
-$ret_post = $_POST;
-$ret_get = $_GET;
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+header('Content-Type: text/html; charset=iso');
+header('Content-Type: application/json');
 
-// $request = file_get_contents('php://input');
-// $json = json_decode($request);
+include "./UnicapApi.php";
 
 if(isset($_GET['token'])){
-    $token = $_GET['token'];
-    $opts = array('http' =>
-        array(
-            'method'  => 'GET',
-            'header'  => array ('Content-Type: application/x-www-form-urlencoded','Referer: https://www1.unicap.br/pergamum3/Pergamum/biblioteca_s/meu_pergamum/index.php?flag=index.php','Cookie: '.$token)
-        )
-    );
-
-    $context  = stream_context_create($opts);
-    $result = file_get_contents('https://www1.unicap.br/pergamum3/Pergamum/biblioteca_s/meu_pergamum/emp_renovacao.php', false, $context);
-
-    print_r($http_response_header);
-    print_r($result);
+    $api = new UnicapApi();
+    $retorno = $api->getLivros($_GET['token']);
+    echo $retorno;
 }
 ?>
